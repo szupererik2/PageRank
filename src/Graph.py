2c781ea2138e84ac523ffe64661cdf18e0361fb7
szupererik2 <szegedierik04@gmail.com>
2025-10-12 16:52:13 +0300

    Add out_degree and in_degree methods to Graph class

--- src/Graph.py
+++ src/Graph.py
@@ -18,3 +18,15 @@ class Graph:
             raise NodeNotFoundError(f'Destination node {dest} does not exist')
         
         self.adj_list[src].append(dest)
+
+    def out_degree(self, node):
+        if node not in self.adj_list:
+            raise NodeNotFoundError(f'Selected node {node} does not exist')
+        
+        return len(self.adj_list[node])
+    
+    def in_degree(self, node):
+        if node not in self.adj_list:
+            raise NodeNotFoundError(f'Selected node {node} does not exist')
+        
+        return [src for src, dests in self.adj_list.items() if node in dests]
