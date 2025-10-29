9bd38c1ffa53727a8f77731d438b48e37de43954
szupererik2 <szegedierik04@gmail.com>
2025-10-29 11:29:56 +0200

    Implement visualization method in Graph class and add test for it

--- src/Graph.py
+++ src/Graph.py
@@ -1,11 +1,9 @@
-class NodeNotFoundError(Exception):
-    pass
+import os
+
 
 class Graph:
-    adj_list = {}
-
     def __init__(self):
-        pass
+        self.adj_list = {}
 
     def add_node(self, node_name):
         self.adj_list.setdefault(node_name, [])
@@ -15,20 +13,29 @@ class Graph:
             self.add_node(src)
         if dest not in self.adj_list:
             self.add_node(dest)
-        
         self.adj_list[src].append(dest)
 
-    def out_degree(self, node):
-        if node not in self.adj_list:
-            raise NodeNotFoundError(f'Selected node {node} does not exist')
-        
-        return list(self.adj_list[node])
-    
-    def in_degree(self, node):
-        if node not in self.adj_list:
-            raise NodeNotFoundError(f'Selected node {node} does not exist')
-        
-        return [src for src, dests in self.adj_list.items() if node in dests]
+    def visualise(self, backend: str | None = None):
+        try:
+            import matplotlib
+            if backend:
+                matplotlib.use(backend)
+            else:
+                if os.name == 'posix' and not os.environ.get('DISPLAY'):
+                    matplotlib.use('Agg')
+            import matplotlib.pyplot as plt
+            import networkx as nx
+        except Exception:
+            print("visualise: skipped (networkx/matplotlib not available)")
+            return
 
-    def get_nodes(self):
-        return list(self.adj_list.keys())
+        G = nx.DiGraph()
+        for src, dests in self.adj_list.items():
+            for dest in dests:
+                G.add_edge(src, dest)
+
+        pos = nx.spring_layout(G)
+        nx.draw(G, pos, with_labels=True, node_color="skyblue", node_size=2000,
+                arrowstyle="->", arrowsize=20, font_size=12, font_weight="bold")
+        plt.show()
+        plt.close()
